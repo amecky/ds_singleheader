e74f6687964552b669e537e1d2301df5f37c1fdb
meckya <amecky@gmail.com>
2018-01-31 20:25:27 +0100

    strect_buffer beta version

--- ds_stretchbuffer/ds_stretchbuffer.h
+++ ds_stretchbuffer/ds_stretchbuffer.h
@@ -1,41 +1,63 @@
 #pragma once
-
+// -----------------------------------------------------------------
+//
+// Based on https://github.com/nothings/stb/blob/master/stretchy_buffer.h
+// written by Sean Barret (https://nothings.org/)
+//
+// Another nice article about this:
+// http://ourmachinery.com/post/minimalist-container-library-in-c-part-1/
+//
+// -----------------------------------------------------------------
 struct array_header {
 	unsigned int capacity;
 	unsigned int size;
 };
 
-#define array_free(a)         ((a) ? free(stb__sbraw(a)),0 : 0)
-#define array_push(a,v)       (stb__sbmaybegrow(a,1), (a)[stb__sbn(a)++] = (v))
-#define array_count(a)        ((a) ? stb__sbn(a) : 0)
-#define array_add(a,n)        (stb__sbmaybegrow(a,n), stb__sbn(a)+=(n), &(a)[stb__sbn(a)-(n)])
-#define array_last(a)         ((a)[stb__sbn(a)-1])
+#define array_free(a) \
+	((a) ? free(array_header(a)),0 : 0)
 
-#define stb__sbraw(a) ((int *) (a) - 2)
-#define stb__sbm(a)   stb__sbraw(a)[0]
-#define stb__sbn(a)   stb__sbraw(a)[1]
+#define array_header(a) \
+    ((array_header*)((char *)(a) - sizeof(array_header)))
 
-#define stb__sbneedgrow(a,n)  ((a)==0 || stb__sbn(a)+(n) >= stb__sbm(a))
-#define stb__sbmaybegrow(a,n) (stb__sbneedgrow(a,(n)) ? stb__sbgrow(a,n) : 0)
-#define stb__sbgrow(a,n)      (*((void **)&(a)) = stb__sbgrowf((a), (n), sizeof(*(a))))
+#define array_size(a) \
+    ((a) ? (array_header((a))->size) : 0)
+
+#define array_capacity(a) \
+    ((a) ? (array_header((a))->capacity) : 0)
+
+#define array_full(a) \
+    ((a) ? (array_size(a) == array_capacity(a)) : true)
+
+#define array_push(a,v) \
+	(array__requires_grow(a,1), \
+	(a)[array_header((a))->size++] = (v))
+
+#define array_remove(a,v) \
+	(array_size((a)) > 0 ? (a)[v] = (a)[array_header((a))->size-1], --array_header((a))->size : 0 )
+
+#define array_last(a) \
+	((a)[array_size(a)-1])
+
+#define array__requires_grow(a,n) \
+	(array_full((a)) ? array__grow(a,n) : 0)
+
+#define array__grow(a,n) \
+	(*((void **)&(a)) = array__growf((a), (n), sizeof(*(a))))
 
 #include <stdlib.h>
 
-static void * stb__sbgrowf(void *arr, int increment, int itemsize) {
-	int dbl_cur = arr ? 2 * stb__sbm(arr) : 0;
-	int min_needed = array_count(arr) + increment;
+static void * array__growf(void *arr, int increment, int itemsize) {
+	int dbl_cur = arr ? 2 * array_capacity(arr) + 8 : 8;
+	int min_needed = array_size(arr) + increment;
 	int m = dbl_cur > min_needed ? dbl_cur : min_needed;
-	int *p = (int *)realloc(arr ? stb__sbraw(arr) : 0, itemsize * m + sizeof(int) * 2);
+	char* p = (char*)realloc(arr ? array_header(arr) : 0, itemsize * m + sizeof(array_header));
 	if (p) {
-		if (!arr)
-			p[1] = 0;
-		p[0] = m;
-		return p + 2;
+		array_header* h = (array_header*)p;
+		if (!arr) {
+			h->size = 0;
+		}
+		h->capacity = m;
+		return p + sizeof(array_header);
 	}
-	else {
-#ifdef STRETCHY_BUFFER_OUT_OF_MEMORY
-		STRETCHY_BUFFER_OUT_OF_MEMORY;
-#endif
-		return (void *)(2 * sizeof(int)); // try to force a NULL pointer exception later
-	}
+	return 0;
 }
--- ds_stretchbuffer/unit_tests/StretchBufferTests.cpp
+++ ds_stretchbuffer/unit_tests/StretchBufferTests.cpp
@@ -5,10 +5,30 @@ struct TestData {
 	int i;
 	float v;
 };
+
 TEST_CASE("Push single value", "[ds_stretchbuffer]") {
 	TestData* data = NULL;
 	TestData td = { 100,1.23f };
 	array_push(data, td);
-	REQUIRE(array_count(data) == 100);
+	REQUIRE(array_size(data) == 1);
+	REQUIRE(array_capacity(data) == 8);
+	array_header* h = array_header(data);
+	REQUIRE(array_size(data) == h->size);
+	REQUIRE(array_capacity(data) == h->capacity);
+	REQUIRE_FALSE(array_full(data));
+	array_free(data);
+}
+
+TEST_CASE("Remove single value", "[ds_stretchbuffer]") {
+	TestData* data = NULL;
+	TestData td = { 100,1.23f };
+	array_push(data, td);
+	TestData nd = { 200, 2.34f };
+	array_push(data, nd);
+	REQUIRE(array_size(data) == 2);
+	REQUIRE(array_capacity(data) == 8);
+	array_remove(data, 0);
+	REQUIRE(array_size(data) == 1);
+	REQUIRE(data[0].i == 200);
 	array_free(data);
 }
